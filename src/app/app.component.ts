9f1ca7b99a64176dda1ed4d26fc8f4aa61548b14
Sultanefendi <sedredinovich@gmail.com>
2023-12-17 15:19:43 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ThemesService } from './services/themesService';
+import { ControlValueAccessor, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'test-project';
+  myForm: FormGroup = new FormGroup({
+    name: new FormControl('Denis'),
+  });
+
+  click() {
+    return 'f';
+  }
+
+  themesService = inject(ThemesService);
+  themes = this.themesService.themes;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { MatExpansionModule} from '@angular/material/expansion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent ],
+  imports: [CommonModule, MatExpansionModule, RouterModule, BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
+  bootstrap: [AppComponent],
+  providers: [
+],
 })
-export class AppModule { }
+export class AppModule {
+}
